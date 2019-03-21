a69df165ff965c17db3aa4767a0d304eeee95dbf
Andrew Waller <andrewhwaller@gmail.com>
2019-03-21 04:43:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,3 +7,11 @@ class CashRegister
     @cash_register = cash_register
   end
 end
+
+#1. each register is a new set of transactions
+#2. on init, cash register can either take a discount or not (if customer is employee, discount, if not, no discount?)
+#3. on init, cash register sets total to 0
+#4. adding items increases total for the transaction
+#5. items can have different quantities
+#6. discount is applied at the end of the transaction
+#7. 
